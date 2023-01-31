91b88cfd742af181dc19026419f315ffd9878095
Kira <sci.bedomohamed2018@alexu.edu.eg>
2023-02-01 01:30:23 +0200

    handle case in cart

--- lib/ui/screens/nav_screens/cart_screen.dart
+++ lib/ui/screens/nav_screens/cart_screen.dart
@@ -12,62 +12,37 @@ class CartScreen extends StatelessWidget {
     final cart = context.watch<CartProvider>().cart;
     return cart == null
         ? const LoadingWidget()
-        : LayoutBuilder(
-            builder: (context, constraints) => SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                    minWidth: constraints.maxWidth,
-                    minHeight: constraints.maxHeight),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 14),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ...cart?.products?.map(
-                                (product) =>
-                                    ChangeNotifierProvider<ProductProvider>(
-                                  create: (_) => ProductProvider(product),
-                                  child: Column(
-                                    children: const [
-                                      CartCard(),
-                                      SizedBox(
-                                        height: 12,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ) ??
-                              [],
-                          // const CartCard(),
-                          // const SizedBox(
-                          //   height: 12,
-                          // ),
-                          // const CartCard(),
-                          // const SizedBox(
-                          //   height: 12,
-                          // ),
-                          // const CartCard(),
-                          // const SizedBox(
-                          //   height: 12,
-                          // ),
-                          Divider(
-                            height: 1,
-                            color: Theme.of(context).cardColor,
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+        : cart.products?.isEmpty ?? true
+            ? Center(
+                child: Text(
+                  'No Products',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineLarge
+                      ?.copyWith(fontSize: 16, fontWeight: FontWeight.w500),
                 ),
-              ),
-            ),
-          );
+              )
+            : SingleChildScrollView(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: cart.products!
+                      .map(
+                        (product) => ChangeNotifierProvider<ProductProvider>(
+                          create: (_) => ProductProvider(product),
+                          child: Column(
+                            children: const [
+                              CartCard(),
+                              SizedBox(
+                                height: 12,
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              );
   }
 }
